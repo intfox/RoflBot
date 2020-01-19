c4b43ab82bab91816d8e48f36371eb9a7ff6a2ec
Roman <initfox@gmail.com>
2020-01-19 08:22:20 +0700

    wtf changes

--- bot.js
+++ bot.js
@@ -13,7 +13,6 @@ const bot = new VkBot({
     confirmation: secretConfig.bot.confirmation,
 })
 
-
 bot.event('message_new', (ctx) => {
     if(ctx.message.text.length > 0) {
         rofl.roflMessage(ctx.message.text).then(
@@ -23,25 +22,29 @@ bot.event('message_new', (ctx) => {
                     for(let reqMes of new Set(requestMessage)) {
                         if(Math.random() < config.random_rate) {
                             ctx.reply(reqMes)
+                            return
                         }
                     }
                 }
-            }
-        ).catch( (reason) => console.error("rofl catch", reason) )
-        if(ctx.message.text.toLowerCase().includes('бот')) {
+            }, reason => console.error("rofl catch", reason))
+        if(ctx.message.text.toLowerCase().split(' ').includes('бот')) {
             analyse.sentiments(ctx.message.text).then( score => {
                 console.log("score:", score, " text:", ctx.message.text)
                 if(score < 0.5) ctx.reply('...', 'photo-190470534_457239018')
-                else { rofl.kakKakat(ctx.message.text).then( k => { if(k) ctx.reply('а как какать') } ) }
-            })
-            
+            }, err => console.log("error sentiments", err))
+        }
+        if(ctx.message.text.trim().toLowerCase() == "да" || ctx.message.text.trim().toLowerCase() == "да." ) {
+            if(Math.random() < 0.05) ctx.reply("Пизда")
+        }
+        if(ctx.message.text.trim().toLowerCase() == "нет" || ctx.message.text.trim().toLowerCase() == "нет.") {
+            if(Math.random() < 0.05) ctx.reply("Пидора ответ")
         }
     }
     if(ctx.message.action && ctx.message.action.type == "chat_invite_user") {
         bot.execute('users.get', {
             user_ids: ctx.message.from_id,
             fields: 'domain'
-        }).then( response => ctx.reply( "@" + response[0].domain, 'doc-190470534_531281679') ).catch( error => console.error("error user.get:", error.message) )
+        }).then( response => ctx.reply( "@" + response[0].domain, 'doc-190470534_531281679'), error => console.error("error user.get:", error.message))
     }
 })
 
@@ -49,7 +52,24 @@ app.use(bodyParser.json())
 
 app.post('/', bot.webhookCallback)
 
-app.listen(80).on('error', (error) => {
+let botServer = app.listen(80)
+
+botServer.on('error', (error) => {
     bot.sendMessage(125160186, 'Смерть.' + error.message)
     bot.sendMessage(2000000002, 'Я упал', 'photo-190470534_457239017')
+})
+
+botServer.on('close', () => {
+    console.log("close")
+    bot.execute('groups.disableOnline', {group_id: config.group_id}).then( () => process.exit(), () => process.exit())
+})
+
+botServer.on('listening', () => {
+    console.log("open")
+    bot.execute('groups.enableOnline', {group_id: config.group_id}).catch( (err) => console.error("error set online", err) )
+})
+
+process.on('SIGINT', () => {
+    console.log("SIGINT")
+    botServer.close()
 })
